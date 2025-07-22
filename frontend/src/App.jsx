d008d7a3c662e2bfedf03f0b39a0232e042715f6
Karina Arichavala <karina.arichavala@epn.edu.ec>
2025-07-22 17:19:51 -0500

    añadir estructura frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">¡PetCare con Bootstrap!</h1>
+      <button className="btn btn-success">Crear Solicitud</button>
+    </div>
+  );
+}
+export default App;
